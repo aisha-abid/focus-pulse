b674a0ba45b858ac411c09e8fdf97a3bfed93346
aisha-abid <ashimughal687@gmail.com>
2025-05-22 04:40:04 +0500

    sve user input and checklist state to localstorage

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ const goalInput=document.querySelectorAll('.goal-input');
 const progressBar=document.querySelector('.progress-bar');
      
 
+const allGoals=JSON.parse(localStorage.getItem('allGoals')) || {
+    // first:{
+    //     name:'',
+    //     completed:false
+    // },
+    // second:{
+    //     name:'',
+    //     completed:false
+    // },
+    // third: {
+    //     name:'',
+    //     completed:false
+    // }
+}
 
 checkBoxList.forEach((checkbox)=>{
     checkbox.addEventListener('click',(e)=>{
@@ -12,6 +26,10 @@ checkBoxList.forEach((checkbox)=>{
         })
         if(allFilled){
             checkbox.parentElement.classList.toggle('completed');
+            const inputId=checkbox.nextElementSibling.id;
+            allGoals[inputId].completed=!allGoals[inputId].completed;
+
+            localStorage.setItem('allGoals', JSON.stringify(allGoals));
 
         }
         else{
@@ -22,7 +40,33 @@ checkBoxList.forEach((checkbox)=>{
 
 
 goalInput.forEach((input)=>{
+
+    if(allGoals[input.id]){
+   input.value=allGoals[input.id].name;
+      if(allGoals[input.id].completed){
+ input.parentElement.classList.add('completed');
+    }
+    }
+
     input.addEventListener('focus',()=>{
         progressBar.classList .remove('show-error');
     })
+    input.addEventListener('input',(e)=>{
+        if(allGoals[input.id]&& allGoals[input.id].completed){
+            input.value=allGoals[input.id].name;
+            return
+        }
+        if(allGoals[input.id]){
+            allGoals[input.id].name=input.value;
+        }
+        else{
+            allGoals[input.id]={
+                name:input.value,
+                completed:false,
+            }
+        }
+            localStorage.setItem('allGoals', JSON.stringify(allGoals));
+
+    })
+
 })
