b496df6790b8f316757b8e7b143516e861904680
aisha-abid <ashimughal687@gmail.com>
2025-05-22 03:53:39 +0500

    Added checkbox logic with if and else for showing error

---
+++ script.js
@@ -0,0 +1,21 @@
+const checkBoxList=document.querySelectorAll('.custom-checkbox');
+const goalInput=document.querySelectorAll('.goal-input');
+const progressBar=document.querySelector('.progress-bar');
+     
+
+
+checkBoxList.forEach((checkbox)=>{
+    checkbox.addEventListener('click',(e)=>{
+        console.log("clicked");
+        const allFilled=[...goalInput].every((input)=>{
+            return input.value;
+        })
+        if(allFilled){
+            checkbox.parentElement.classList.toggle('completed');
+
+        }
+        else{
+        progressBar.classList.add('show-error');
+        }
+    })
+})
